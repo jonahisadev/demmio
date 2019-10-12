f75e3c961e85505c0ac87c99a8c0036ff35ae0da
Jonah Alligood <jonahisadev@gmail.com>
2019-10-12 18:09:08 -0500

    Images display with linear interpolation

--- src/Nodes/ImageNode.cpp
+++ src/Nodes/ImageNode.cpp
@@ -11,4 +11,5 @@ void ImageNode::render(int width, int height) {
 
 void ImageNode::setImage(std::string path) {
     _image = std::make_shared<JEngine::TexturedQuad>(_pos.x(), _pos.y(), _size.x(), _size.y(), path.c_str());
+    _image->linearInterp(true);
 }
